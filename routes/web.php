931b9bcb9079ea73c1b6613c69c39d2f67169167
ArielMejiaDev <arielmejiadev@gmail.com>
2026-03-27 00:00:27 -0600

    add issue

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::inertia('/', 'welcome', [
 ])->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboar', 'dashboard')->name('dashboard');
+    Route::inertia('dashboard', 'dashboar')->name('dashboard');
 });
 
 require __DIR__.'/settings.php';
